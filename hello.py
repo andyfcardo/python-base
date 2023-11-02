9e5f8489e1e884265014db47b0f8c8382ee60a03
anderson <anderson.f.cardoso85@outlook.com>
2023-11-01 21:24:57 -0300

    Alteracao de variaveis de linguagem

--- hello.py
+++ hello.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python3
-#Este programa imprime Hello World
-print("Hello, World!") #test-ignore
+
+"""Hello World Multi Linguas
+Dependendo da lingua configurada no ambiente o programa exibe
+a mensagem correspondente.
+
+Como usar:
+
+Tena a variavel Lang devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execucao:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "afcardo"
+__license__ ="Unlicense"
+
+import os
+
+current_language = os.getenv ("LANG", "en_US")[:5]
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, mundo!"
+
+print(msg)
