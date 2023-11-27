4df6d5b2b09c6ec45ef2d9bc92cb81366b345108
anderson <anderson.f.cardoso85@outlook.com>
2023-11-27 08:25:34 -0300

    New versions

--- escola_v2_com_sets.py
+++ escola_v2_com_sets.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-
-"""Exibe relatório de crianças por atividade
+"""Exibe relatório de crianças por atividade.
 
 Imprimir a lista de crianças agrupadas por sala
-que frequenta cada uma das atividades
+que frequentas cada uma das atividades.
 """
-__version__ = "0.1.0",
+__version__ = "0.1.0"
 
 # Dados
-
 sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
 sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
 
@@ -17,8 +15,8 @@ aula_musica = ["Erik", "Carlos", "Maria"]
 aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
 
 atividades = [
-    ("Ingles", aula_ingles),
-    ("Música", aula_musica,) ,
+    ("Inglês", aula_ingles), 
+    ("Música", aula_musica), 
     ("Dança", aula_danca),
 ]
 
@@ -26,15 +24,17 @@ atividades = [
 
 for nome_atividade, atividade in atividades:
 
-    atividade_sala1 = []
-    atividade_sala2 = []
+    print(f"Alunos da atividade {nome_atividade}\n")
+    print("-" * 40)
 
-    for aluno in atividade:
-        if aluno in sala1:
-            atividade_sala1.append(aluno)
-        elif aluno in sala2:
-            atividade_sala2.append(aluno)
-print(f"{nome_atividade} sala1", atividade_sala1)
-print(f"{nome_atividade} sala2", atividade_sala2)
+# Sala 1 que tem intersecao com a ativdade
+    atividade_sala1 = set(sala1) & set(atividade)
+    atividade_sala2 = set(sala2) & set(atividade)
+
+    print("Sala1 ", atividade_sala1)
+    print("Sala2 ", atividade_sala2)
+
+    print()
+    print("#" * 40)
 
 
--- hello.py
+++ hello.py
@@ -16,20 +16,44 @@ Execucao:
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "afcardo"
 __license__ ="Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv ("LANG", "en_US")[:5]
-msg = "Hello, World!"
+arguments = {"lang": None, "count": 1}
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language == "es_SP":
-    msg = "Hola, mundo!"
+for arg in sys.argv[1:]:
+    #TODO: Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option {key}")
+        sys.exit()
+    arguments[key] = value
 
-print(msg)
+
+current_language = arguments["lang"]
+if current_language is None:
+    #TODO: Usar repeticao
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language:")
+        
+current_language = current_language[:5]
+    
+msg = {
+    "en_US": "Hello, World",
+    "pt_BR": "Olá, Mundo!",
+     "it_IT": "Ciao, Mondo!",
+    "es_SP": "Hola, mundo!",
+    "fr_FR": "Bonjour, monde!",
+}
+
+print(
+    msg[current_language] * int(arguments["count"])
+)
